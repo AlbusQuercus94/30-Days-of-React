35843fd662c44505fbcb9b5eafd92f6da6932ee0
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-11-12 23:53:18 -0300

    Update Dia 02 Script
    Atualização do script do dia 02 com alguns comentários extras do assunto.

--- Exercícios/Dia_02/script.js
+++ Exercícios/Dia_02/script.js
@@ -6,3 +6,35 @@ class HelloWorld extends React.Component{
     }
 }
 ReactDOM.HelloWorld.render();
+
+/*O mesmo componente acima, mas escrito de uma forma sem misturar o HTML e o JS
+
+class HelloWorl extends React.Component{
+    render(){
+        return(
+            React.createElement(
+                'h1',
+                {className: 'large'},
+                'Hello World'
+            )
+        );
+    }
+}
+
+*/
+
+/*Outra comparação de como escrever no HTML e como escrever usando o React.
+
+HTML:
+    <div>
+        <img src='img.png' alt='Source Img' />
+        <h1> Hello Pedro </h1>
+    <div>
+
+React:
+    React.createElement('div', null,
+        React.createElement('img', {src: 'img.png, alt: 'Source Img'}),
+        React.createElement('h1', null, 'Hello Pedro')
+    );
+
+*/
